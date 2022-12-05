13ffb79b1dbd597372aa142d618f4e93dabc16d8
deepali kumari <111889695+kumariDeepali@users.noreply.github.com>
2022-12-05 16:36:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+//import '../styles/App.css';
+const App = () => {
+
+  const [data,setData] = useState(true)
+
+  
+  return (
+    <div id="main">
+
+    <h1 id={"marco-polo"} >{data ? "Marco":"Polo"}</h1>
+    <button id='marco-polo-toggler' onClick={()=>setData(!data)}>{!data ? "Marco":"Polo"}</button>
+
+    </div>
+  )
+}
+
+
+export default App;
