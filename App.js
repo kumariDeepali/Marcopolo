8fc6c5e5cd1c58a6efd77e12a86bdc1a30a1460b
deepali kumari <111889695+kumariDeepali@users.noreply.github.com>
2022-12-08 21:20:58 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
-//import '../styles/App.css';
+import React from "react";
+//import "../styles/App.css";
+import { useState } from "react";
+
 const App = () => {
+  const [buttons, setButtons] = useState(true);
 
-  const [data,setData] = useState(true)
+  const handleClick = () =>
+    setButtons((prevstate) => {
+      return !prevstate;
+    });
 
-  
   return (
     <div id="main">
-
-    <h1 id={"marco-polo"} >{data ? "Marco":"Polo"}</h1>
-    <button id='marco-polo-toggler' onClick={()=>setData(!data)}>{!data ? "Marco":"Polo"}</button>
-
+      <h1 id="marco-polo">{buttons ? "Marco" : "Polo"}</h1>
+      <button id="marco-polo-toggler" onClick={handleClick}>
+        {buttons ? "Polo" : "Marco"}
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
